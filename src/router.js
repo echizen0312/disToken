036632b6146263c3b40df7ba538fc0dba89fe22a
Izayoi Sakuya <sakuya0312@qq.com>
2018-09-29 13:26:49 +0800

    1.0.0

--- src/router.js
+++ src/router.js
@@ -11,15 +11,15 @@ import QrCode from './components/QrCode.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {path: '/', name: 'Home', component: AccountList},
-    {path: '/Other', name: 'Other', component: Other},
-    {path: '/AccountList', name: 'AccountList', component: AccountList},
-    {path: '/Account/:id', name: 'Account', component: Account},
-    {path: '/Transfer/:id/:token', name: 'Transfer', component: Transfer},
-    {path: '/TransferList/:id/:token', name: 'TransferList', component: TransferList},
-    {path: '/Web', name: 'Web', component: Web},
-    {path: '/Tracker/:trx_id', name: 'Tracker', component: Web},
-    {path: '/QrCode/:id', name: 'QrCode', component: QrCode}
-  ]
+    routes: [
+        {path: '/', name: 'Home', component: AccountList},
+        {path: '/Other', name: 'Other', component: Other},
+        {path: '/AccountList', name: 'AccountList', component: AccountList},
+        {path: '/Account/:id', name: 'Account', component: Account},
+        {path: '/Transfer/:id/:token', name: 'Transfer', component: Transfer},
+        {path: '/TransferList/:id/:token', name: 'TransferList', component: TransferList},
+        {path: '/Web', name: 'Web', component: Web},
+        {path: '/Tracker/:trx_id', name: 'Tracker', component: Web},
+        {path: '/QrCode/:id', name: 'QrCode', component: QrCode}
+    ]
 })
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  baseUrl: '/disToken',
-  // baseUrl: '/hpw',
-  // baseUrl: '/wallet',
-  assetsDir: 'static',
-  productionSourceMap: false,
-  lintOnSave: undefined
+    baseUrl: '/disToken',
+    // baseUrl: '/hpw',
+    // baseUrl: '/wallet',
+    assetsDir: 'static',
+    productionSourceMap: false,
+    lintOnSave: undefined
 }
